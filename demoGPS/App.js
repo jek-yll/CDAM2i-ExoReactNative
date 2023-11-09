3ac9d54414440f0a9845a1edd438e068cd0bfe5e
loic <loic.gstrn@gmail.com>
2023-11-09 16:59:13 +0100

    ExoGPS

--- demoGPS/App.js
+++ demoGPS/App.js
@@ -1,20 +1,23 @@
 import { Button, StyleSheet, Text, View } from 'react-native'
 import React, { useEffect, useState } from 'react'
-import Geolocation from './@react-native-community/geolocation'
+import Geolocation from '@react-native-community/geolocation'
 import axios from 'axios'
+import { APIKEY } from './api';
+
 
 // npm i @react-native-community/geolocation
-// ajout de la ligne : android/app/src/main/AndroidManifest.xml
-// <uses-permission android:name="android.permission.ACCESS_FINE_LOCATION" /
+// ajout de la ligne : <uses-permission android:name="android.permission.ACCESS_FINE_LOCATION" />
+// dans le repertoire android/app/src/main/AndroidManifest.xml
+// npm i axios
 
-const API_KEY = "aG858mYDyms6xcHCYKFFcxgzQcsPUlYG"
 
 export default function App() {
 
     const [latitude, setLatitude] = useState(null)
     const [longitude, setLongitude] = useState(null)
-    const [getPosition, setGetPosition] = useState(true)
-    const [localisationFetch, setLocalisationFetch] = useState()
+    const [getPosition,setGetPosition] = useState(true)
+    const [localisationFetch,setLocalisationFetch] = useState({pays : "", ville: ""})
+    const [localisationAxios,setLocalisationAxios] = useState({pays : "", ville: ""})
 
     useEffect(() => {
         Geolocation.requestAuthorization()
@@ -22,39 +25,41 @@ export default function App() {
             setLatitude(position.coords.latitude)
             setLongitude(position.coords.longitude)
         },
-            error => console.log(error),
-            { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+        error => console.log(error),
+        {enableHighAccuracy : true, timeout : 20000,maximumAge : 1000 }
         )
-    }, [getPosition])
+    },[getPosition])
 
-    const reload = () => {
-        setGetPosition(state => !state)
+    function getApiWithFetch(){
+      fetch(`http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=${APIKEY}&q=${latitude}%2C${longitude}`)
+      .then(response => response.json())
+      .then(data => setLocalisationFetch({pays : data.Country.LocalizedName, ville: data.LocalizedName}))
+      .catch(error => console.log(error))
     }
 
-    const getMaLoc = async () => {
-        try {
-            const response = await axios.get(`http://dataservice.accuweather.com/locations/v1/cities/geoposition/searchsearch?apikey=${API_KEY}=${latitude}%2C${longitude}`)
-            console.log(response.data);
-            // setMaVille(response.data...)
-        } catch (error) {
-            console.error(error.message)
-        }
+    function getApiWithAxios(){
+      axios.get(`http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=${APIKEY}&q=${latitude}%2C${longitude}`)
+      .then(response => setLocalisationAxios({pays : response.data.Country.LocalizedName, ville: response.data.LocalizedName}))
+      .catch(error => console.log(error))
 
     }
 
+    function reload(){
+      setGetPosition(state => !state)
+    }
 
-    return (
-        <View>
-            <Text>latitude : {latitude} </Text>
-            <Text>longitude : {longitude} </Text>
-            <Text>ma ville : {maVille}</Text>
-            <Button 
-                title='Refresh'
-                onPress={reload}
-            />
-            <Button title='ma ville'></Button>
-        </View>
-    )
+
+  return (
+    <View>
+      <Button title='refresh' onPress={reload}></Button>
+      <Text>Latitude : {latitude}</Text>
+      <Text>Longitude : {longitude}</Text>
+      <Button title='Get localisation withFetch' onPress={getApiWithFetch}></Button>
+      <Text>Localisation : {localisationFetch.pays} {localisationFetch.ville}</Text>
+      <Button title='Get localisation withAxios' onPress={getApiWithAxios}></Button>
+      <Text>Localisation : {localisationAxios.pays} {localisationAxios.ville}</Text>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({})
